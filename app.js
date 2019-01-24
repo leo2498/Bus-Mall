3f6232c0ee6c0ef6bf5d08b25bcf3d74e0fb43b0
Leo Juarez <juarezleo9@gmail.com>
2019-01-24 10:29:20 -0800

    adds tab icon with edits to code

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ if (localStorage.getItem('storedProducts') !== null) {
   localStorage.setItem('storedProducts', JSON.stringify(Product.allProducts));
 }
 
-function randomProduct() {
+function randomProduct(){
   var randomOne = Math.floor(Math.random() * Product.allProducts.length);
   var randomTwo = Math.floor(Math.random() * Product.allProducts.length);
   var randomThree = Math.floor(Math.random() * Product.allProducts.length);
@@ -76,4 +76,112 @@ function randomProduct() {
   Product.lastDisplayed[0] = randomOne;
   Product.lastDisplayed[1] = randomTwo;
   Product.lastDisplayed[2] = randomThree;
+
+
+function newSet (event) {
+
+  if (event.target.id === 'productsSection') {
+    return alert('Please click on an image.');
+  }
+}
+  Product.totalVotes--;
+
+  for(var i = 0; i < Product.allProducts.length; i++) {
+    if(event.target.altText === Product.allProducts[i].altText) {
+      Product.allProducts[i].votes++;
+      updateChartArrays();
+    }
+  }
+
+  if (Product.totalVotes < 1) {
+    Product.section.removeEventListener('click', newSet);
+    productsSection.innerHTML = '';
+    localStorage.setItem('storedProducts', JSON.stringify(Product.allProducts));
+    drawChart();
+  }
+  randomProduct();
+}
+
+function updateChartArrays() {
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    names[i] = Product.allProducts[i].name;
+    votes[i] = Product.allProducts[i].votes;
+  }
+}
+var data = {
+  labels: names,
+  datasets: [
+    {
+      label: 'Votes per Product',
+      data: votes,
+      backgroundColor: [
+        'rgb(255,18,0)',
+        'rgb(232,62,0)',
+        'rgb(255,117,0)',
+        'rgb(232,146,0)',
+        'rgb(255,195,0)',
+        'rgb(255,205,0)',
+        'rgb(232,218,0)',
+        'rgb(192,255,0)',
+        'rgb(68,232,0)',
+        'rgb(0,255,35)',
+        'rgb(0,255,112)',
+        'rgb(0,232,189)',
+        'rgb(0,210,255)',
+        'rgb(0,111,232)',
+        'rgb(0,33,255)',
+        'rgb(0,80,255)',
+        'rgb(131,0,232)',
+        'rgb(206,0,255)',
+        'rgb(232,0,195)',
+        'rgb(255,0,83)'
+      ],
+
+      hoverBackgroundColor: [
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon',
+        'lemonchiffon'
+      ]
+    }]
+};
+
+function drawChart() {
+  var ctx = document.getElementById('productStats').getContext('2d');
+  new Chart(ctx,{
+    type: 'doughnut',
+    data: data,
+    options: {
+      responsive: false,
+      animation: {
+        duration: 1000,
+        easing: 'easeOutBounce'
+      }
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          max: 20,
+          min: 0,
+          stepSize: 1.0
+        }
+      }]
+    }
+  });
 }
