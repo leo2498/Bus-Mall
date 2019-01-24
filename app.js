67a541f94f7972acd019d1efa21bc0457c55e4fa
Leo Juarez <juarezleo9@gmail.com>
2019-01-23 20:07:16 -0800

    adds function

--- app.js
+++ app.js
@@ -14,3 +14,12 @@ Product.resultsList = document.getElementById('resultsList');
 var productOne = document.getElementById('productOne');
 var productTwo = document.getElementById('productTwo');
 var productThree = document.getElementById('productThree');
+
+function Product(name, filepath, altText) {
+  this.name = name;
+  this.filepath = filepath;
+  this.altText = altText;
+  this.votes = 0;
+  this.views = 0;
+  Product.allProducts.push(this);
+}
