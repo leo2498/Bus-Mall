f6fd331cfc2eed5c80c08236b4df5d18076833f8
Leo Juarez <juarezleo9@gmail.com>
2019-01-23 20:21:06 -0800

    adds random display and increments for imgs

--- app.js
+++ app.js
@@ -23,3 +23,57 @@ function Product(name, filepath, altText) {
   this.views = 0;
   Product.allProducts.push(this);
 }
+
+new Product('Luggage', 'img/bag.jpg','Bag');
+new Product('Banana Slicer', 'img/banana.jpg','Banana Slicer');
+new Product('Bathroom Buddy', 'img/bathroom.jpg','Bathroom');
+new Product('Fashion Wellies', 'img/boots.jpg','Boots');
+new Product('Breakfast Machine', 'img/breakfast.jpg','Breakfast');
+new Product('Meatball Bubblegum', 'img/bubblegum.jpg', 'Bubblegum');
+new Product('Chair', 'img/chair.jpg', 'Chair');
+new Product('Cthulhu', 'img/cthulhu.jpg', 'Cthulhu');
+new Product('Duck Muzzle', 'img/dog-duck.jpg', 'Dog Duck');
+new Product('Dragon Meat', 'img/dragon.jpg', 'Dragon Meat');
+new Product('Practical Cutlery', 'img/pen.jpg', 'Pen');
+new Product('Pet Broom', 'img/pet-sweep.jpg', 'Pet Broom');
+new Product('Pizza Scissors', 'img/scissors.jpg', 'Scissors');
+new Product('Shark Attack', 'img/shark.jpg', 'Shark');
+new Product('Baby Broom', 'img/sweep.png', 'Sweep');
+new Product('Tauntaun', 'img/tauntaun.jpg', 'Tauntaun');
+new Product('Unicorn Meat', 'img/unicorn.jpg', 'Unicorn Meat');
+new Product('Tentacle USB', 'img/usb.gif', 'Tentacle USB');
+new Product('Watering Can', 'img/water-can.jpg', 'Watering Can');
+new Product('Wine Glass', 'img/wine-glass.jpg', 'Wine Glass');
+
+if (localStorage.getItem('storedProducts') !== null) {
+  console.log('Data found');
+  Product.allProducts = JSON.parse(localStorage.getItem('storedProducts'));
+} else {
+  console.log('Not found');
+  localStorage.setItem('storedProducts', JSON.stringify(Product.allProducts));
+}
+
+function randomProduct() {
+  var randomOne = Math.floor(Math.random() * Product.allProducts.length);
+  var randomTwo = Math.floor(Math.random() * Product.allProducts.length);
+  var randomThree = Math.floor(Math.random() * Product.allProducts.length);
+  while(Product.lastDisplayed.includes(randomOne) || Product.lastDisplayed.includes(randomTwo) || Product.lastDisplayed.includes(randomThree) || randomOne === randomTwo || randomTwo == randomThree || randomThree == randomOne) {
+    randomOne = Math.floor(Math.random() * Product.allProducts.length);
+    randomTwo = Math.floor(Math.random() * Product.allProducts.length);
+    randomThree = Math.floor(Math.random() * Product.allProducts.length);
+  }
+  productOne.src = Product.allProducts[randomOne].filepath;
+  productTwo.src = Product.allProducts[randomTwo].filepath;
+  productThree.src = Product.allProducts[randomThree].filepath;
+  productOne.altText = Product.allProducts[randomOne].altText;
+  productTwo.altText = Product.allProducts[randomTwo].altText;
+  productThree.altText = Product.allProducts[randomThree].altText;
+
+  Product.allProducts[randomOne].views++;
+  Product.allProducts[randomTwo].views++;
+  Product.allProducts[randomThree].views++;
+
+  Product.lastDisplayed[0] = randomOne;
+  Product.lastDisplayed[1] = randomTwo;
+  Product.lastDisplayed[2] = randomThree;
+}
